34eba05503a40978dc85235ec70fde767a0ed439
glows777 <1914426389@qq.com>
2023-10-27 10:02:47 +0800

    chore: rxjs-demo

--- packages/others/jest.config.js
+++ packages/others/jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  "testMatch": [
+    "**/src/**/test/*.spec.ts"
+  ],
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
